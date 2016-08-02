ebba1a57be2d44e355ab7f1e492576400224f0dd
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-08-02 22:32:57 +0900

    Fix to be configurable

--- Server/AccServer.rb
+++ Server/AccServer.rb
@@ -2,10 +2,16 @@ require 'eventmachine'
 require 'em-websocket'
 require 'serialport'
 
+SERIALPORT_DEVICE = ENV["SERIALPORT_DEVICE"] || "/dev/ttyACM0"
+SERIALPORT_BAUDRATE = ENV["SERIALPORT_BAUDRATE"]&.to_i || 9600
+
+HOST = ENV["HOST"] || "0.0.0.0"
+PORT = ENV["PORT"]&.to_i || 8000
+
 EM.run do
   connections = []
 
-  EM.attach SerialPort.new(ENV["SERIAL_PORT_DEVICE"], 38400, 8, 1, 0) do |sp|
+  EM.attach SerialPort.new(SERIALPORT_DEVICE, SERIALPORT_BAUDRATE, 8, 1, 0) do |sp|
     sp.define_singleton_method :receive_data do |data|
       connections.each do |conn|
         conn.send data
@@ -13,7 +19,7 @@ EM.run do
     end
   end
 
-  EM::WebSocket.start host: "0.0.0.0",port: 8000 do |ws|
+  EM::WebSocket.start host: HOST, port: PORT do |ws|
     ws.onopen do
       connections << ws
     end
